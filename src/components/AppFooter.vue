6c22e0c13fac61c0442b90e728f901348fc62faf
pannellalino <lista>
2022-11-26 18:23:42 +0100

    add footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,9 +6,69 @@ export default {
 </script>
 
 <template>
-  
+  <footer class="text-center p-5">
+    <div class="container pb-5">
+      <div class="row">
+        <div class="offset-1 col-3">
+          <ul class="text-start">
+            <li><strong class="text-dark">Address</strong></li>
+            <li>382 NE 191st # 87394 Miami, FL 33179-3899</li>
+            <li>+1(305) 547-9909 (9am - 5pm EST,Monday -Friday)</li>
+            <li>support@maxcoach.com</li>
+          </ul>
+        </div>
+        <div class="offset-1 col-2">
+          <ul class="text-start">
+            <li><strong class="text-dark">Explore</strong></li>
+            <li>Start here</li>
+            <li>Blog</li>
+            <li>About us</li>
+          </ul>
+        </div>
+        <div class="col-2">
+          <ul class="text-start">
+            <li>&nbsp</li>
+            <li>Success story</li>
+            <li>Courses</li>
+            <li>Contact us</li>
+          </ul>
+        </div>
+        <div class="col-2">
+          <ul class="text-start">
+            <li><strong class="text-dark">Information</strong></li>
+            <li>Membership</li>
+            <li>Purchase guide</li>
+            <li>Privacy policy</li>
+            <li>Terms of services</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <span class="text-secondary">2020 Maxcoach. All Rights Reserved</span>
+    <button class="rounded-circle"><i class="fa-solid fa-arrow-up"></i></button>
+  </footer>
 </template>  
 
 <style lang="scss" scoped>
-
+footer{
+  background-color: #f6f3f6;
+  position: relative;
+  span{
+    font-size: .8rem;
+  }
+  li:not(strong){
+    color: grey;
+  }
+  button{
+    border: 0;
+    position: absolute;
+    bottom: 20px;
+    right: 20px;
+    width: 50px;
+    height: 50px;
+    background-color: #27ab94;
+    color: white;
+    font-size: 20px;
+  }
+}
 </style>
